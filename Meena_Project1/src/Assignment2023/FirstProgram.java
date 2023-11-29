f0fe17939d73a9772e3e54335ffe0eec5d9ce9fa
HP <HP@10.0.0.9>
2023-11-29 09:44:03 -0500

    first_commit

---
+++ Meena_Project1/src/Assignment2023/FirstProgram.java
@@ -0,0 +1,10 @@
+package Assignment2023;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Meena Shaikh, Welcome to Selenium Assignments.");
+	}
+
+}
