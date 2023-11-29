8defcab97779059a1ba5fddda6da7a85d68c8863
HP <HP@10.0.0.9>
2023-11-29 13:29:47 -0500

    Hello World

---
+++ Meena_Project1/src/Assignment2023/HelloWorld.java
@@ -0,0 +1,11 @@
+package Assignment2023;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World, Welcome to Selenium World:");
+System.out.println("Topics :" + "1. DataTypes" + "2. Operators");
+	}
+
+}
---
+++ Meena_Project1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module Meena_Project1 {
+}
