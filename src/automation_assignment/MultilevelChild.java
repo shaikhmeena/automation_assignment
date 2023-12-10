79096d2b8654fa2852975fd5b3bb6e4778e4af9e
Meena Shaikh <shaikhmeena>
2023-12-09 21:05:29 -0500

    Can you prove Inheritance is unidirectional

--- src/automation_assignment/MultilevelChild.java
+++ src/automation_assignment/MultilevelChild.java
@@ -19,6 +19,19 @@ public class MultilevelChild extends MultilevelParent{
 		c1.Bike();//MultilevelChild object can access method of MultilevelParent class.
 		c1.FarmHouse();
 		c1.Car();
+		MultilevelGrandParent gp1= new MultilevelGrandParent();
+		gp1.Shop(); //MultilevelGrandParent object can access method of MultilevelGranParent class
+		//gp1.House();//MultilevelGrandParent object cannot access method of MultilevelParent class.
+		//gp1.Bike();//MultilevelGrandParent object cannot access method of MultilevelParent class.
+		//gp1.FarmHouse();//MultilevelGrandParent object cannot access method of MultilevelChild class.
+		//gp1.Car();//MultilevelGrandParent object cannot access method of MultilevelChild class.
+		MultilevelParent p1= new MultilevelParent();
+		p1.House(); //MultilevelParent object can access method of MultilevelParent class.
+		p1.Bike(); //MultilevelParent object can access method of MultilevelParent class.
+		p1.Shop();//MultilevelParent object can access method of MultilevelGranParent class.
+		//p1.FarmHouse(); //MultilevelParent object cannot access method of MultilevelChild class.
+		//p1.Car();//MultilevelParent object cannot access method of MultilevelChild class.
+	
 	}
 
 }
